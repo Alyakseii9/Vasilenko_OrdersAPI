9b808b6dd03c5d7231febbb0f99e0429e502f610
Alexei_1966 <Alexei_1966@outlook.com>
2023-03-29 19:45:11 +0300

    +new class

--- Vasilenko_OrdersAPI/Models/Order.cs
+++ Vasilenko_OrdersAPI/Models/Order.cs
@@ -15,30 +15,5 @@ namespace Vasilenko_OrdersAPI.Models
         { 
         
         }
-        /*   Spyrgyash
-    public class Order
-    {
-        public Guid Id { get; set; }
-        public decimal Total { get; set; }
-        public Guid BuyerId { get; set; }
-        public List<ProductItem> ProductItems { get; set; }
-    }
-         *   
-         *   */
-        /*  Matskevitch
-        public class Order
-    {
-        public Guid Id { get; set; }
-        public string Buyer { get; set; }
-        public decimal Total { get; set; }
-        public List<Product> Products { get; set; } = new List<Product>();
-        public Order()
-        {
-            Id = Guid.NewGuid();
-        }        
-    }
-         * 
-         * 
-         * */
     }
 }
